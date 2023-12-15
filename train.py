3bfd68241a40c5603e13ef6977b2f14c7e2e9cfc
Neverwell <117388341+iamNeverwell@users.noreply.github.com>
2023-12-15 12:28:15 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,68 @@
+import argparse
+import torch
+from collections import OrderedDict
+from os.path import isdir
+from torch import nn
+from torch import optim
+from torchvision import datasets, transforms, models
 
+def arg_parser():
+    # ... (unchanged)
+
+def train_transformer(train_dir):
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                [0.229, 0.224, 0.225])])
+    train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+    return train_data
+
+def test_transformer(test_dir):
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize([0.485, 0.456, 0.406], 
+                                                               [0.229, 0.224, 0.225])])
+    test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
+    return test_data
+
+def data_loader(data, train=True):
+    if train: 
+        loader = torch.utils.data.DataLoader(data, batch_size=50, shuffle=True)
+    else: 
+        loader = torch.utils.data.DataLoader(data, batch_size=50)
+    return loader
+
+def check_gpu(gpu_arg):
+    # ... (unchanged)
+
+def primaryloader_model(architecture="vgg16"):
+    model = models.vgg16(pretrained=True)
+    model.name = "vgg16"
+    
+    for param in model.parameters():
+        param.requires_grad = False 
+    return model
+
+def initial_classifier(model, hidden_units):
+    # ... (unchanged)
+
+def validation(model, testloader, criterion, device):
+    # ... (unchanged)
+
+def network_trainer(model, trainloader, validloader, device, criterion, optimizer, epochs, print_every, steps):
+    # ... (unchanged)
+
+def validate_model(model, testloader, device):
+    # ... (unchanged)
+
+def initial_checkpoint(model, save_dir, train_data):
+    # ... (unchanged)
+
+def main():
+    # ... (unchanged)
+
+if __name__ == '__main__':
+    main()
