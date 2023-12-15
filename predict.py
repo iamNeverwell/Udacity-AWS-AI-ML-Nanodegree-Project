23d25b55ced82725fd3a2f45323228cd42e2ad31
Neverwell <117388341+iamNeverwell@users.noreply.github.com>
2023-12-15 16:06:37 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,100 +1,123 @@
-import argparse
-import json
-import PIL
-import torch
-import numpy as np
-from math import ceil
-from train import check_gpu, initial_classifier, validation
+checkpoint = 'vgg16_bn_checkpoint.pth'
 
-def arg_parser():
-    parser = argparse.ArgumentParser(description="predict.py")
-    parser.add_argument('--image', type=str, help='Point to image file for prediction.', required=True)
-    parser.add_argument('--checkpoint', type=str, help='Point to checkpoint file as str.', required=True)
-    parser.add_argument('--top_k', type=int, help='Choose top K matches as int.')
-    parser.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
-    parser.add_argument('--gpu', default="gpu", action="store", dest="gpu")
+def load_checkpoint(filepath,device):
+    if device=="gpu":
+        map_location=lambda device, loc: device.cuda()
+    else:
+        map_location='cpu'
+        
+    checkpoint = torch.load(f=filepath,map_location=map_location)
 
-    args = parser.parse_args()
-    
-    return args
+    return checkpoint['model_arch'],checkpoint['clf_input'], checkpoint['clf_output'], checkpoint['clf_hidden'],checkpoint['state_dict'],checkpoint['model_class_to_index']
 
-def load_checkpoint(checkpoint_path):
-    checkpoint = torch.load("checkpoint.pth")
-    
-    model = models.vgg16(pretrained=True)
-    model.name = "vgg16"
-    
-    for param in model.parameters():
-        param.requires_grad = False
-    
-    model.class_to_idx = checkpoint['class_to_idx']
-    model.classifier = checkpoint['classifier']
-    model.load_state_dict(checkpoint['state_dict'])
-    
-    return model
+model_arch,input_units, output_units, hidden_units, state_dict, class_to_idx = load_checkpoint(checkpoint,device)
+model.load_state_dict(state_dict)
+
+practice_img = './flowers/test/58/image_02677.jpg'
 
 def process_image(image):
-    img = PIL.Image.open(image)
+    processed_image = Image.open(image).convert('RGB') 
+    processed_image.thumbnail(size=(256,256)) 
+    width, height = processed_image.size
 
-    original_width, original_height = img.size
-    
-    if original_width < original_height:
-        size = [256, 256**600]
-    else: 
-        size = [256**600, 256]
+    new_width,new_height = 224,224 
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+    processed_image = processed_image.crop((left, top, right, bottom))
+
+    transf_tens = transforms.ToTensor()
+    transf_norm = transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
+    tensor = transf_norm(transf_tens(processed_image))
+
+    np_processed_image = np.array(tensor)
+    return np_processed_image
+
+def imshow(image, ax=None, title=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+        plt.tick_params(
+            axis='both',          
+            which='both',     
+            bottom=False,      
+            top=False,
+            left=False,         
+            labelbottom=False,
+            labelleft=False,)
         
-    img.thumbnail(size)
-   
-    center = original_width/4, original_height/4
-    left, top, right, bottom = center[0]-(244/2), center[1]-(244/2), center[0]+(244/2), center[1]+(244/2)
-    img = img.crop((left, top, right, bottom))
-
-    numpy_img = np.array(img)/255 
-
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    numpy_img = (numpy_img - mean) / std
-        
-    numpy_img = numpy_img.transpose(2, 0, 1)
+    image = image.transpose((1, 2, 0))
     
-    return torch.from_numpy(numpy_img).type(torch.FloatTensor)
 
-def predict(image_path, model, device, cat_to_name, topk=5):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+from PIL import Image
+
+processed_image = Image.open(practice_img)
+processed_image
+imshow(process_image(practice_img))
+
+file = 'cat_to_name.json'
+
+def class_to_label(file,classes):
+    with open(file, 'r') as f:
+        class_mapping =  json.load(f)
+        
+    labels = []
+    for c in classes:
+        labels.append(class_mapping[c])
+    return labels
+
+
+idx_mapping = dict(map(reversed, class_to_idx.items()))
+
+def predict(image_path, model,idx_mapping, topk, device):
+    pre_processed_image = torch.from_numpy(process_image(image_path))
+    pre_processed_image = torch.unsqueeze(pre_processed_image,0).to(device).float()
+    
+    model.to(device)
     model.eval()
     
-    image_tensor = process_image(image_path)
-    image_tensor = image_tensor.unsqueeze(0)
-    image_tensor = image_tensor.to(device)
+    log_ps = model.forward(pre_processed_image)
+    ps = torch.exp(log_ps)
+    top_ps,top_idx = ps.topk(topk,dim=1)
+    list_ps = top_ps.tolist()[0]
+    list_idx = top_idx.tolist()[0]
+    classes = []
+    model.train()
     
-    with torch.no_grad():
-        output = model.forward(image_tensor)
-    
-    probabilities, indices = torch.topk(output, topk)
-    probabilities = probabilities.exp()
+    for x in list_idx:
+        classes.append(idx_mapping[x])
+    return list_ps, classes
 
-    class_to_idx_inv = {model.class_to_idx[key]: key for key in model.class_to_idx}
-    classes = [cat_to_name[class_to_idx_inv[idx]] for idx in indices[0]]
+def print_predictions(probabilities, classes,image,category_names=None):
+    print(image)
     
-    return probabilities[0].tolist(), indices[0].tolist(), classes
+    if category_names:
+        labels = class_to_label(category_names,classes)
+        for i,(ps,ls,cs) in enumerate(zip(probabilities,labels,classes),1):
+            print(f'{i}) {ps*100:.2f}% {ls.title()} | Class No. {cs}')
+    else:
+        for i,(ps,cs) in enumerate(zip(probabilities,classes),1):
+            print(f'{i}) {ps*100:.2f}% Class No. {cs} ')
+    print('') 
 
-def print_probability(probs, flowers):
-    for i, j in enumerate(zip(flowers, probs)):
-        print("Rank {}:".format(i+1),
-              "Flower: {}, likelihood: {}%".format(j[1], ceil(j[0]*100)))
+probabilities,classes = predict(practice_img,model,idx_mapping,5,device)
 
-def main():
-    args = arg_parser()
-    
-    with open(args.category_names, 'r') as f:
-        cat_to_name = json.load(f)
+print_predictions(probabilities,classes,practice_img.split('/')[-1],file)
 
-    model = load_checkpoint(args.checkpoint)
-    
-    device = check_gpu(gpu_arg=args.gpu)
-    
-    top_probs, top_labels, top_flowers = predict(args.image, model, device, cat_to_name, args.top_k)
-    
-    print_probability(top_probs, top_flowers)
+imshow(process_image(practice_img))
 
-if __name__ == '__main__':
-    main()
+plt.figure() # image
+plt.barh(class_to_label(file,classes),width=probabilities)
+plt.title('Model Predictions') 
+plt.gca().invert_yaxis()
+plt.show()
